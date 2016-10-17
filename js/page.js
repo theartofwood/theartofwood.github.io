09623a77f16bc2c0900f7fa35a9f52e1987b9839
Sven Hüser <s.hueser@gmail.com>
2016-10-17 22:09:06 +0200

    lightbox navigation

--- js/page.js
+++ js/page.js
@@ -9,30 +9,37 @@ $('#mobile-menu').bind('click', toggleMenu);
 $('#mobile-overlay').bind('click', toggleMenu);
 
 // Lightbox
-
-function bind(scope, fn) {
-    return function() { fn.apply(scope, arguments); };
+function mod(n, m) {
+  return ((n % m) + m) % m;
 }
 
 var lightboxLinks = $('a[data-rel="lightbox"]');
 
 if (lightboxLinks.length) {
-  var lightbox = $('<div id="lightbox">').appendTo('body');
-
-  var spinner = $('<div class="spinner">')
-    .append($('<div class="bounce1">'))
-    .append($('<div class="bounce2">'))
-    .append($('<div class="bounce3">'))
-    .append($('<div class="bounce4">'));
+  var lightbox   = $('<div id="lightbox">');
+  var leftArrow  = $('<div class="arrow left">');
+  var rightArrow = $('<div class="arrow right">');
 
   var lightboxImages;
+
   var buildLightbox = function() {
     if (!lightboxImages) {
+
+      lightbox.appendTo('body')
+        .append(leftArrow)
+        .append(rightArrow);
+
       lightboxLinks.each(function() {
+        var spinner = $('<div class="spinner">')
+          .append($('<div class="bounce1">'))
+          .append($('<div class="bounce2">'))
+          .append($('<div class="bounce3">'))
+          .append($('<div class="bounce4">'));
+
         var image = $('<div>', {
           'class': 'image-container',
           'data-src': this.href
-        }).append(spinner.clone());
+        }).append(spinner);
         image.appendTo(lightbox);
       });
 
@@ -40,6 +47,9 @@ if (lightboxLinks.length) {
 
       lightboxLinks.bind('click', clickLink);
       lightbox.bind('click', toggleLightbox);
+
+      leftArrow.bind('click', function(e) {nextImage(-1,e)});
+      rightArrow.bind('click', function(e) {nextImage(1,e)});
     }
   }
 
@@ -54,12 +64,12 @@ if (lightboxLinks.length) {
 
     var currentImage = lightboxImages.eq(i).addClass('visible');
     var src = currentImage.attr('data-src');
+    currentImage.removeAttr('data-src');
     if (src) {
       var loadImg = new Image();
       loadImg.src = src;
       loadImg.onload = function() {
         currentImage.html(this);
-        currentImage.removeAttr('data-src');
       }
     }
   }
@@ -73,6 +83,30 @@ if (lightboxLinks.length) {
       showImage(lightboxLinks.index(this));
     }
   }
+
+  var nextImage = function(dir, e) {
+    if (e) e.stopPropagation();
+    var currImage = parseInt(lightbox.attr('data-image'));
+    var nextImage = mod(currImage + dir, lightboxImages.length);
+
+    showImage(nextImage);
+  }
+
+  $(document).bind('keydown', function(e) {
+    if (lightbox.is('.visible')) {
+      switch (e.keyCode) {
+        case 37:
+          nextImage(-1);
+          break;
+        case 39:
+          nextImage(1);
+          break;
+        case 27:
+          toggleLightbox();
+        default:
+      }
+    }
+  });
 
   buildLightbox();
 }
