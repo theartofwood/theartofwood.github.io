636bce1d4820f350968e337f977c57b008dd4e8c
Sven Hüser <s.hueser@gmail.com>
2018-11-07 14:47:16 +0100

    wip

--- galerie.md
+++ galerie.md
@@ -6,11 +6,11 @@ navigation_weight: 3
 
 {% assign galleries = site.galleries | sort: date | reverse %}
 {% for gallery in galleries %}
-  {% assign folder = gallery.path | replace: '_galleries/', '' | replace: '.html', '' %}
+  {% assign folder = gallery.path | replace: '_galleries/', 'galerie/' | replace: '.html', '' %}
   <div class="gallery overview">
     <a href="{{ gallery.url }}">
       <div class="gallery-image small">
-      <img src="/images/galleries/{% if gallery.folder %}{{ gallery.folder }}{% else %}{{ folder }}{% endif %}/small/{{ gallery.overview }}" alt="{{ gallery.title }}">
+      <img src="/{% if gallery.folder %}{{ gallery.folder }}{% else %}{{ folder }}{% endif %}/thumb/{{ gallery.overview }}" alt="{{ gallery.title }}">
       </div>
       <p class="gallery-title">{{ gallery.title }}</p>
     </a>
--- js/page.js
+++ js/page.js
@@ -16,106 +16,317 @@ function mod(n, m) {
   return ((n % m) + m) % m;
 }
 
-var lightboxLinks = $('a[data-rel="lightbox"]');
+// PhotoSwipe.com
+var initPhotoSwipeFromDOM = function(gallerySelector) {
 
-if (lightboxLinks.length) {
-  var lightbox   = $('<div id="lightbox">');
-  var leftArrow  = $('<div class="arrow left">');
-  var rightArrow = $('<div class="arrow right">');
-  var closeCross = $('<div class="close">');
+  console.log(gallerySelector);
+  // parse slide data (url, title, size ...) from DOM elements
+  // (children of gallerySelector)
+  var parseThumbnailElements = function(el) {
+    var thumbElements = el.childNodes,
+        numNodes = thumbElements.length,
+        items = [],
+        figureEl,
+        linkEl,
+        size,
+        item;
 
-  var lightboxImages;
+    for(var i = 0; i < numNodes; i++) {
 
-  var buildLightbox = function() {
-    if (!lightboxImages) {
+      figureEl = thumbElements[i]; // <figure> element
 
-      lightbox.appendTo('body')
-        .append(leftArrow)
-        .append(rightArrow);
-        // .append(closeCross);
+      // include only element nodes
+      if(figureEl.nodeType !== 1) {
+        continue;
+      }
 
-      lightboxLinks.each(function() {
-        var spinner = $('<div class="spinner">')
-          .append($('<div class="bounce1">'))
-          .append($('<div class="bounce2">'))
-          .append($('<div class="bounce3">'))
-          .append($('<div class="bounce4">'));
+      linkEl = figureEl.children[0]; // <a> element
+      console.log(linkEl);
 
-        var image = $('<div>', {
-          'class': 'image-container',
-          'data-src': this.href
-        }).append(spinner);
-        image.appendTo(lightbox);
-      });
+      size = linkEl.getAttribute('data-size').split('x');
+      console.log(linkEl.getAttribute('href'));
 
-      lightboxImages = $('.image-container');
+      // create slide object
+      item = {
+        src: 'med/' + linkEl.getAttribute('data-name'),
+        w: parseInt(size[0], 10),
+        h: parseInt(size[1], 10)
+      };
 
-      lightboxLinks.bind('click', clickLink);
-      lightbox.bind('click', toggleLightbox);
-      // closeCross.bind('click', toggleLightbox);
 
-      leftArrow.bind('click', function(e) {nextImage(-1,e)});
-      rightArrow.bind('click', function(e) {nextImage(1,e)});
+      if(figureEl.children.length > 1) {
+        // <figcaption> content
+        item.title = figureEl.children[1].innerHTML;
+      }
 
-      $(document).bind('keydown', function(e) {
-        if (lightbox.is('.visible')) {
-          switch (e.keyCode) {
-            case 37: // left arrow
-              nextImage(-1);
-              break;
-            case 39: // right arrow
-              nextImage(1);
-              break;
-            case 27: // ESC
-              toggleLightbox();
-            default:
+      if(linkEl.children.length > 0) {
+        // <img> thumbnail element, retrieving thumbnail url
+        item.msrc = linkEl.children[0].getAttribute('src');
+      }
+
+      item.el = figureEl; // save link to element for getThumbBoundsFn
+      items.push(item);
+    }
+
+    return items;
+  };
+
+  // find nearest parent element
+  var closest = function closest(el, fn) {
+    return el && ( fn(el) ? el : closest(el.parentNode, fn) );
+  };
+
+  // triggers when user clicks on thumbnail
+  var onThumbnailsClick = function(e) {
+    e = e || window.event;
+    e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+    var eTarget = e.target || e.srcElement;
+
+    // find root element of slide
+    var clickedListItem = closest(eTarget, function(el) {
+      return (el.tagName && el.tagName.toUpperCase() === 'FIGURE');
+    });
+
+    if(!clickedListItem) {
+      return;
+    }
+
+    // find index of clicked item by looping through all child nodes
+    // alternatively, you may define index via data- attribute
+    var clickedGallery = clickedListItem.parentNode,
+      childNodes = clickedListItem.parentNode.childNodes,
+      numChildNodes = childNodes.length,
+      nodeIndex = 0,
+      index;
+
+    for (var i = 0; i < numChildNodes; i++) {
+      if(childNodes[i].nodeType !== 1) {
+        continue;
+      }
+
+      if(childNodes[i] === clickedListItem) {
+        index = nodeIndex;
+        break;
+      }
+      nodeIndex++;
+    }
+
+
+
+    if(index >= 0) {
+      // open PhotoSwipe if valid index found
+      openPhotoSwipe( index, clickedGallery );
+    }
+    return false;
+  };
+
+  // parse picture index and gallery index from URL (#&pid=1&gid=2)
+  var photoswipeParseHash = function() {
+    var hash = window.location.hash.substring(1),
+        params = {};
+
+    if(hash.length < 5) {
+      return params;
+    }
+
+    var vars = hash.split('&');
+    for (var i = 0; i < vars.length; i++) {
+      if(!vars[i]) {
+        continue;
+      }
+      var pair = vars[i].split('=');
+      if(pair.length < 2) {
+        continue;
+      }
+      params[pair[0]] = pair[1];
+    }
+
+    if(params.gid) {
+      params.gid = parseInt(params.gid, 10);
+    }
+
+    return params;
+  };
+
+  var openPhotoSwipe = function(index, galleryElement, disableAnimation, fromURL) {
+    var pswpElement = document.querySelectorAll('.pswp')[0],
+        gallery,
+        options,
+        items;
+
+    items = parseThumbnailElements(galleryElement);
+
+    // define options (if needed)
+    options = {
+
+      // define gallery index (for URL)
+      galleryUID: galleryElement.getAttribute('data-pswp-uid'),
+
+      getThumbBoundsFn: function(index) {
+        // See Options -> getThumbBoundsFn section of documentation for more info
+        var thumbnail = items[index].el.getElementsByTagName('img')[0], // find thumbnail
+          pageYScroll = window.pageYOffset || document.documentElement.scrollTop,
+          rect = thumbnail.getBoundingClientRect();
+
+        return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+      },
+
+      pinchToClose: false
+
+    };
+
+    // PhotoSwipe opened from URL
+    if(fromURL) {
+      if(options.galleryPIDs) {
+        // parse real index when custom PIDs are used
+        // http://photoswipe.com/documentation/faq.html#custom-pid-in-url
+        for(var j = 0; j < items.length; j++) {
+          if(items[j].pid == index) {
+            options.index = j;
+            break;
           }
         }
-      });
-    }
-  }
-
-  var toggleLightbox = function() {
-    $('body').toggleClass('noscroll');
-    lightbox.toggleClass('visible');
-  }
-
-  var showImage = function(i) {
-    lightbox.attr('data-image', i);
-    lightboxImages.filter('.visible').removeClass('visible');
-
-    var currentImage = lightboxImages.eq(i).addClass('visible');
-    var src = currentImage.attr('data-src');
-    currentImage.removeAttr('data-src');
-    if (src) {
-      var loadImg = new Image();
-      loadImg.src = src;
-      loadImg.onload = function() {
-        currentImage.html(this);
+      } else {
+        // in URL indexes start from 1
+        options.index = parseInt(index, 10) - 1;
       }
+    } else {
+      options.index = parseInt(index, 10);
     }
-  }
 
-  var clickLink = function(e) {
-    if (e.button == 0) {
-      e.preventDefault();
-
-      toggleLightbox();
-
-      showImage(lightboxLinks.index(this));
+    // exit if index not found
+    if( isNaN(options.index) ) {
+      return;
     }
+
+    if(disableAnimation) {
+      options.showAnimationDuration = 0;
+    }
+
+    // Pass data to PhotoSwipe and initialize it
+    gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+  };
+
+  // loop through all gallery elements and bind events
+  var galleryElements = document.querySelectorAll( gallerySelector );
+
+  for(var i = 0, l = galleryElements.length; i < l; i++) {
+    galleryElements[i].setAttribute('data-pswp-uid', i+1);
+    galleryElements[i].onclick = onThumbnailsClick;
   }
 
-  var nextImage = function(dir, e) {
-    if (e) e.stopPropagation();
-    var currImage = parseInt(lightbox.attr('data-image'));
-    var nextImage = mod(currImage + dir, lightboxImages.length);
-
-    showImage(nextImage);
+  // Parse URL and open gallery if it contains #&pid=3&gid=1
+  var hashData = photoswipeParseHash();
+  if(hashData.pid && hashData.gid) {
+    openPhotoSwipe( hashData.pid ,  galleryElements[ hashData.gid - 1 ], true, true );
   }
+};
 
-  buildLightbox();
-}
+// execute above function
+initPhotoSwipeFromDOM('.sh-image-gallery');
+
+// var lightboxLinks = $('a[data-rel="lightbox"]');
+
+// if (lightboxLinks.length) {
+//   var lightbox   = $('<div id="lightbox">');
+//   var leftArrow  = $('<div class="arrow left">');
+//   var rightArrow = $('<div class="arrow right">');
+//   var closeCross = $('<div class="close">');
+
+//   var lightboxImages;
+
+//   var buildLightbox = function() {
+//     if (!lightboxImages) {
+
+//       lightbox.appendTo('body')
+//         .append(leftArrow)
+//         .append(rightArrow);
+//         // .append(closeCross);
+
+//       lightboxLinks.each(function() {
+//         var spinner = $('<div class="spinner">')
+//           .append($('<div class="bounce1">'))
+//           .append($('<div class="bounce2">'))
+//           .append($('<div class="bounce3">'))
+//           .append($('<div class="bounce4">'));
+
+//         var image = $('<div>', {
+//           'class': 'image-container',
+//           'data-src': this.href
+//         }).append(spinner);
+//         image.appendTo(lightbox);
+//       });
+
+//       lightboxImages = $('.image-container');
+
+//       lightboxLinks.bind('click', clickLink);
+//       lightbox.bind('click', toggleLightbox);
+//       // closeCross.bind('click', toggleLightbox);
+
+//       leftArrow.bind('click', function(e) {nextImage(-1,e)});
+//       rightArrow.bind('click', function(e) {nextImage(1,e)});
+
+//       $(document).bind('keydown', function(e) {
+//         if (lightbox.is('.visible')) {
+//           switch (e.keyCode) {
+//             case 37: // left arrow
+//               nextImage(-1);
+//               break;
+//             case 39: // right arrow
+//               nextImage(1);
+//               break;
+//             case 27: // ESC
+//               toggleLightbox();
+//             default:
+//           }
+//         }
+//       });
+//     }
+//   }
+
+//   var toggleLightbox = function() {
+//     $('body').toggleClass('noscroll');
+//     lightbox.toggleClass('visible');
+//   }
+
+//   var showImage = function(i) {
+//     lightbox.attr('data-image', i);
+//     lightboxImages.filter('.visible').removeClass('visible');
+
+//     var currentImage = lightboxImages.eq(i).addClass('visible');
+//     var src = currentImage.attr('data-src');
+//     currentImage.removeAttr('data-src');
+//     if (src) {
+//       var loadImg = new Image();
+//       loadImg.src = src;
+//       loadImg.onload = function() {
+//         currentImage.html(this);
+//       }
+//     }
+//   }
+
+//   var clickLink = function(e) {
+//     if (e.button == 0) {
+//       e.preventDefault();
+
+//       toggleLightbox();
+
+//       showImage(lightboxLinks.index(this));
+//     }
+//   }
+
+//   var nextImage = function(dir, e) {
+//     if (e) e.stopPropagation();
+//     var currImage = parseInt(lightbox.attr('data-image'));
+//     var nextImage = mod(currImage + dir, lightboxImages.length);
+
+//     showImage(nextImage);
+//   }
+
+//   buildLightbox();
+// }
 
 // slideshow
 
